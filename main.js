0de4f47e53ab4dc235028c5fa1e60488836f3180
Zhiling Hu <zh347@cornell.edu>
2020-04-07 17:42:44 -0400

    more identifiable name, availability detection, add to cart url

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 var apiKey;
 try {
-  apiKey = fs.readFileSync('apikey.txt', 'utf8').split('\n')[0];
+    apiKey = fs.readFileSync('apikey.txt', 'utf8').split('\n')[0];
 } catch (e) {
-  console.log('Error:', e);
+    console.log('Error:', e);
 }
 
 const BestBuyAPI = require('bestbuy')(apiKey);
@@ -11,38 +11,47 @@ const BestBuyAPI = require('bestbuy')(apiKey);
 const SKUs = [6364255, 6364253, 6401728];
 
 const CreateSKUsQuery = (skusArr) => {
-  if (!Array.isArray(skusArr)) {
-    throw new Error('Not an array');
-  }
-  param = sku => `SKU=${sku}`
-  skus = skusArr.map(param);
-  return skus.join('|');
+    if (!Array.isArray(skusArr)) {
+        throw new Error('Not an array');
+    }
+    param = sku => `SKU=${sku}`
+    skus = skusArr.map(param);
+    return skus.join('|');
 };
 
 
 var SearchRslts = (prodIds) => {
-  return BestBuyAPI.products(CreateSKUsQuery(prodIds), {
-    show: 'sku,name,onlineAvailability,onlineAvailabilityUpdateDate,orderable,inStoreAvailability,inStoreAvailabilityUpdateDate'
-  })
+    return BestBuyAPI.products(CreateSKUsQuery(prodIds), {
+    show: 'sku,name,onlineAvailability,onlineAvailabilityUpdateDate,orderable,inStoreAvailability,addToCartUrl'
+    })
 }
 
 var count = 1;
+var lastTime = [" "," "," "];
+var lastAvailable = [false, false, false]
 setInterval(function() {
-   SearchRslts(SKUs).then(response => {
-       for (const p of response.products){
-//           console.log(p);
-//           if(p.orderable=='SoldOut')
-//               console.log("SoldOut");
-//           if(p.orderable=='Available' && p.onlineAvailability)
-//               console.log(p.name);
-           if(p.orderable=='Available' && p.onlineAvailability){
-               console.log(p.sku);
-               console.log(p.name);
-           }
-//           else
-//               console.log("Offline Available");
-       }
-       count+=1;
-       console.log(count)
-   }).catch(err => err)
-}, 2000);
+    SearchRslts(SKUs).then(response => {
+        var prods = response.products
+        for (i = 0; i < prods.length; i++){
+            //           console.log(prods[i]);
+            var name = prods[i].name.split(' - ')[2].substring(0, 10);
+            if(prods[i].onlineAvailabilityUpdateDate!=lastTime[i]){
+                lastTime[i] = prods[i].onlineAvailabilityUpdateDate
+                console.log("STOCK UPDATE for " + name + " at " + lastTime[i]);
+            }
+            if(prods[i].onlineAvailability!=lastAvailable[i]){
+                if(!lastAvailable[i]){
+                    console.log(name + " Becomes Available ");
+                    console.log(prods[i].addToCartUrl)
+                }
+                else
+                    console.log(name + " Becomes Unavailable" + prods[i].addToCartUrl)
+                lastAvailable[i]!=lastAvailable[i];
+            }
+        }
+            //            if(prods[i].orderable=='Available')
+            //            if(prods[i].orderable=='SoldOut')
+            count+=1;
+            console.log(count)
+        }).catch(err => err)
+    }, 2000);
