48bca096b23e8d3a797fac42f370552fa199795e
Zhiling Hu <zh347@cornell.edu>
2020-04-14 10:41:47 -0400

    easy run formatting updates

--- EasyRunSwitchInStockTracker.js
+++ EasyRunSwitchInStockTracker.js
@@ -1,14 +1,7 @@
 const open = require('open')
 const BestBuyAPI = require('bestbuy')('');
-const SKUs = [6364255, 6364253, 6401728];
-const CreateSKUsQuery = (skusArr) => {
-    param = sku => `SKU=${sku}`
-    skus = skusArr.map(param);
-    return skus.join('|');
-};
-
-var SearchRslts = (prodIds) => {
-    return BestBuyAPI.products(CreateSKUsQuery(prodIds), {
+var SearchRslts = () => {
+    return BestBuyAPI.products("SKU=6364255|SKU=6364253|SKU=6401728", {
     show: 'sku,name,onlineAvailability,addToCartUrl'
     })
 }
@@ -16,10 +9,10 @@ var LastUpdateStr = [" ", " ", " "];
 var LastOnlineAvailable = [false, false, false];
 var counter = -1;
 function Monitor() {
-    SearchRslts(SKUs).then(response => {
+    SearchRslts().then(response => {
         var prods = response.products
         for (i = 0; i < 3; i++){
-            var name = prods[i].name.split(' - ')[2];
+            var name = prods[i].name.split(' - ')[2].substring(0, 10);
             if(prods[i].onlineAvailability!=LastOnlineAvailable[i]){
                 if(!LastOnlineAvailable[i]){
                     open(prods[i].addToCartUrl)
@@ -34,12 +27,12 @@ function Monitor() {
         }counter+=1;
         if(counter%50==0){
             for (i = 0; i < 3; i++){
-                var pStr = (new Date).toLocaleTimeString() + " " + prods[i].name.split(' - ')[2] + " is ["
-                if(!prods[i].onlineAvailability)
-                    pStr+="NOT "
-                    pStr+= "AVAILABLE] for online shipping."
-                    console.log(pStr)
-                    }
+                var pStr = (new Date).toLocaleTimeString() + " " + prods[i].name.split(' - ')[2].substring(0, 10) + " is ["
+                if(!prods[i].onlineAvailability){
+                    pStr+="NOT "}
+                pStr+= "AVAILABLE] for online shipping."
+                console.log(pStr)
+            }
             
         }
     }).catch(err => err)
