2ac22f507f23d86e2a72314c0a49629c48861769
Zhiling Hu <zh347@cornell.edu>
2020-04-13 20:10:34 -0400

    updated the print out for the easy run script
    took out the online date availability as it could be confusing

--- EasyRunSwitchInStockTracker.js
+++ EasyRunSwitchInStockTracker.js
@@ -9,40 +9,42 @@ const CreateSKUsQuery = (skusArr) => {
 
 var SearchRslts = (prodIds) => {
     return BestBuyAPI.products(CreateSKUsQuery(prodIds), {
-    show: 'sku,name,onlineAvailability,onlineAvailabilityUpdateDate,addToCartUrl'
+    show: 'sku,name,onlineAvailability,addToCartUrl'
     })
 }
-var LastUpdateStr = [" "," "," "];
-var LastOnlineAvailable = [false, false, false];;
+var LastUpdateStr = [" ", " ", " "];
+var LastOnlineAvailable = [false, false, false];
 var counter = -1;
 function Monitor() {
     SearchRslts(SKUs).then(response => {
         var prods = response.products
-        for (i = 0; i < prods.length; i++){
-            var name = prods[i].name.split(' - ')[2].substring(0, 10);
+        for (i = 0; i < 3; i++){
+            var name = prods[i].name.split(' - ')[2];
             if(prods[i].onlineAvailability!=LastOnlineAvailable[i]){
                 if(!LastOnlineAvailable[i]){
                     open(prods[i].addToCartUrl)
-                    console.log(name + " becomes available.");
+                    console.log((new Date).toLocaleTimeString() + " " + name + " is available.");
+                    console.log("Paste the link into a browser if needed again: " + prods[i].addToCartUrl)
                 }
                 else{
-                    console.log(name + " becomes unavailable.")
+                    console.log((new Date).toLocaleTimeString() + " " + name + " becomes unavailable.")
                 }
                 LastOnlineAvailable[i]=!LastOnlineAvailable[i];
             }
-            
-            if(prods[i].onlineAvailabilityUpdateDate!=LastUpdateStr[i]){
-                if(LastOnlineAvailable[i]){
-                    open(prods[i].addToCartUrl)
-                }
-                LastUpdateStr[i] = prods[i].onlineAvailabilityUpdateDate
-                console.log("Latest online stock update for " + name + " at " + LastUpdateStr[i]);
-            }
         }counter+=1;
-        if(counter%20==0){
-            console.log("Running")}
-    }).catch(err => {
-        console.error('Error Message: ' + err.message);
-    })
+        if(counter%50==0){
+            for (i = 0; i < 3; i++){
+                var pStr = (new Date).toLocaleTimeString() + " " + prods[i].name.split(' - ')[2] + " is ["
+                if(!prods[i].onlineAvailability)
+                    pStr+="NOT "
+                    pStr+= "AVAILABLE] for online shipping."
+                    console.log(pStr)
+                    }
+            
+        }
+    }).catch(err => err)
 }
 var monitor = setInterval(Monitor, 2000);
+console.log((new Date).toLocaleDateString() + " " + (new Date).toLocaleTimeString())
+console.log("Start Running")
+console.log("The program is checking with BestBuy API every two seconds. \nA list of products online availability should be printed out around every 100 seconds for your own reference.")
